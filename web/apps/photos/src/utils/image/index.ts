093315e4e6a14f0e56a3d8768e77d3472a60e3fb
laurenspriem <github_uni.fishers@aleeas.com>
2024-04-06 12:45:30 +0530

    [web] ML bilinear interpolation util functions

--- web/apps/photos/src/utils/image/index.ts
+++ web/apps/photos/src/utils/image/index.ts
@@ -4,6 +4,58 @@ import { BlobOptions, Dimensions } from "types/image";
 import { enlargeBox } from "utils/machineLearning";
 import { Box } from "../../../thirdparty/face-api/classes";
 
+export function normalizePixelBetween0And1(pixelValue: number) {
+    return pixelValue / 255.0;
+}
+
+export function getPixelData(imageData: Uint8ClampedArray, width: number, x:number, y:number) {
+    const index = (y * width + x) * 4;
+    return {
+      r: imageData[index],
+      g: imageData[index + 1],
+      b: imageData[index + 2],
+      a: imageData[index + 3]
+    };
+  }
+
+export function clamp(value: number, min: number, max: number) {
+    return Math.min(max, Math.max(min, value));
+}
+
+/// Returns the pixel value (RGB) at the given coordinates using bilinear interpolation.
+export function getPixelBilinear(fx: number, fy: number, imageData: Uint8ClampedArray, imageWidth: number, imageHeight: number) {
+    // Clamp to image boundaries
+    fx = clamp(fx, 0, imageWidth - 1);
+    fy = clamp(fy, 0, imageHeight - 1);
+
+    // Get the surrounding coordinates and their weights
+    const x0 = Math.floor(fx);
+    const x1 = Math.ceil(fx);
+    const y0 = Math.floor(fy);
+    const y1 = Math.ceil(fy);
+    const dx = fx - x0;
+    const dy = fy - y0;
+    const dx1 = 1.0 - dx;
+    const dy1 = 1.0 - dy;
+
+    // Get the original pixels 
+    const pixel1 = getPixelData(imageData, imageWidth, x0, y0);
+    const pixel2 = getPixelData(imageData, imageWidth, x1, y0);
+    const pixel3 = getPixelData(imageData, imageWidth, x0, y1);
+    const pixel4 = getPixelData(imageData, imageWidth, x1, y1);
+
+    function bilinear(val1: number, val2: number, val3: number, val4: number) {
+        return Math.round(val1 * dx1 * dy1 + val2 * dx * dy1 + val3 * dx1 * dy + val4 * dx * dy);
+    }
+
+    // Interpolate the pixel values
+    const red = bilinear(pixel1.r, pixel2.r, pixel3.r, pixel4.r);
+    const green = bilinear(pixel1.g, pixel2.g, pixel3.g, pixel4.g);
+    const blue = bilinear(pixel1.b, pixel2.b, pixel3.b, pixel4.b);
+
+    return { r: red, g: green, b: blue };
+}
+
 export function resizeToSquare(img: ImageBitmap, size: number) {
     const scale = size / Math.max(img.height, img.width);
     const width = scale * img.width;
